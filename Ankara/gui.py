048eda249886956d0adfecb85648cfb1931ef793
esince <esinece6@gmail.com>
2019-09-06 14:33:09 +0300

    esc changes

--- Ankara/gui.py
+++ Ankara/gui.py
@@ -64,11 +64,14 @@ class GUI(Frame):
         value = 152
         row_value = 1
 
-        button_text=" Choose a folder"
+        button_text=" Choose a Folder"
 
         if LabelName == 'Balance':
             self.company_name = Entry(frame,width = 25)
             self.balance_date = Entry(frame,width=25)
+
+            self.company_name.insert(END,'JKE Global')
+            self.balance_date.insert(END,'06.09.2019')
             Label(frame,text=" Date:").grid(row = 3, column = 0,sticky=W)
             Label(frame,text=" Company Name:").grid(row = 2, column = 0, sticky=W)
             self.company_name.grid(row = 2, column = 1,ipady=3, columnspan = 7,sticky=EW,pady=(0,10),padx=(0,3))
@@ -76,26 +79,37 @@ class GUI(Frame):
 
             value = 0
             row_value = 4
-            button_text = "Choose a file"
+            button_text = "Choose a File"
 
         Button(frame,text=button_text,command = lambda : self.ChooseDirec(cmd_type),width = 13).grid(row = row_value , column = 0, columnspan = 2,sticky=W)
         Button(frame,text='Run',command = lambda : self.Run(cmd_type),font=("", "9", "bold"),width=15).grid(row = row_value , column = 7, columnspan = 1, sticky=E+W,padx=(value,0),pady=(5,5))
 
 
     def ChooseDirec(self,opType):
-        self.company_name.delete(0,END)
-        self.balance_date.delete(0,END)
+        print ('1')
         self.count.config(text='')
+        print ('2')
+
         if opType == 1:
-            self.fileDialog = tkFileDialog.askdirectory(title = "Select a Directory:")
+            print ('3')
+            #self.company_name.delete(0,END)
+            #self.balance_date.delete(0,END)
+            self.fileDialog = tkFileDialog.askdirectory(title = 'Select a Directory:')
+            #self.fileDialog = tkFileDialog.askdirectory(title = "Select a Directory:")
+            print ('222')
         elif opType == 0:
             self.fileDialog = tkFileDialog.askopenfilename(title = "Select An Excel File",filetypes=[("Excel files", "*.xlsx")])
             self.direc_path = os.path.dirname(self.fileDialog)
+            print ('4')
+
             self.file_name = os.path.basename(self.fileDialog)
+        print ('5')
 
         self.DirecPath.config(state='normal')
         self.DirecPath.delete(0,END)
         self.DirecPath.insert(END,self.fileDialog)
+        print ('6')
+
         if opType == 1:
             self.getFiles()
             self.label_string('Files count: ')
@@ -126,7 +140,7 @@ class GUI(Frame):
         saveName = self.saveName.get()
 
         if ("." in saveName): # prevent the dots in the save name
-            Msg.showerror("Name Error",'Please Write A Name Without " . "')
+            Msg.showerror("Name Error",'Please write a name without " . "')
 
 
         elif(self.DirecPath.get()==''):
@@ -140,11 +154,11 @@ class GUI(Frame):
             saveName += ".xlsx"
             if opType == 1:
                 if os.path.isfile(self.fileDialog):
-                    Msg.showwarning('Wrong path',"The chosen path if not compatible with the operation, Please check and try again.")
+                    Msg.showwarning('Wrong Path',"The chosen path if not compatible with the operation, please check and try again.")
                     return
 
                 elif (len(self.__files)==0):
-                    Msg.showwarning("Notice", "The Path You Chose Does Not Contain Any Excel Files!")
+                    Msg.showwarning("Notice", "The path you chose does not contain any excel files!")
 
                 else:
                     self.update_count_label(len(self.__files))
@@ -152,11 +166,13 @@ class GUI(Frame):
             elif opType == 0:
                 pattern = re.compile("^[0-9]{2}.[0-9]{2}.[0-9]{4}$")
 
-                if (pattern.search(self.balance_date.get())==None):
+                if (self.company_name.get().strip()==""):
+                    Msg.showwarning("Notice","You can't leave the company name field empty.")
+
+                elif (pattern.search(self.balance_date.get())==None):
                     Msg.showwarning("Notice","Please follow the format DD.MM.YYYY")
 
-                elif (self.company_name.get().strip()==""):
-                    Msg.showwarning("Notice","You can't leave the company name field empty.")
+
                 else:
                     self.block_entries('disabled')
                     self.update_count_label(self.file_name+"==>"+saveName)
@@ -191,7 +207,9 @@ class GUI(Frame):
         self.update_idletasks()
         self.__files = [os.path.join(self.fileDialog, file) for file in self.__files]
 
-
+    def save_as(self,savename):
+        self.label_string('Status: ')
+        self.update_count_label('Saved as ' + savename)
     def calcualte(self,saveName,opType):
         self.saveName.config(state='disabled')
         try:
@@ -199,33 +217,36 @@ class GUI(Frame):
                 self.updateGui()
                 ExcelObject = Excel(self.__files, self.fileDialog, saveName)
                 ExcelObject.loadFiles()
-                self.update_count_label("Saved as "+saveName)
+                
 
             elif opType == 0:
                 self.update_idletasks()
                 ExcelObject = Excel(files=[],path= self.fileDialog,saveAs=saveName,b_date=self.balance_date.get(),b_name=self.company_name.get())
                 ExcelObject.bilanco()
-            Msg.showinfo("Notice","Done, Please check your directory.")
+            self.save_as(saveName)
+            Msg.showinfo("Notice","Done, please check your directory.")
 
 
         except KeyError as E:
             self.error_state()
-            Msg.showwarning('Notice',"The file"+str(E)+"does not follow the accepted format.")
-
+            Msg.showwarning('Notice',"The input file "+ str(E) + " does not follow the accepted format.")
+        except ValueError as E:
+            print(E)
+            self.error_state()
+            Msg.showwarning('Notice',"The input file "+str(E)+" does not follow the accepted format.")
         except PermissionError as E:
             self.error_state()
             Msg.showwarning('Notice',str(E))
 
-        except Exception as E:
+        except Exception as E:            
             self.error_state()
-            Msg.showwarning('Error','Error on line {}'.format(sys.exc_info()[-1].tb_lineno)+" "+type(E).__name__+"\n"+
-                            "Please send the input files with a screen shot of this window to get help.")
+            Msg.showwarning('Error','Error on line {}'.format(sys.exc_info()[-1].tb_lineno)+" "+type(E).__name__+"\n" + str(E) +"\n\nPlease send the input files with a screenshot of this window to get help.")
         finally:
             self.saveName.config(state='normal')
             self.block_entries('normal')
 
     def error_state(self):
-        self.update_count_label('Failed')
+        self.update_count_label('Failed!')
         self.label_string('Status:')
 
 
